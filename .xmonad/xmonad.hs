648b495e7a95cc8f5ba19136b1e95402a77718ee
Mindaugas Celiesius <m.celiesius@yandex.ru>
2019-10-06 22:04:57 +0300

    Cleaned xmonad config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,11 +2,7 @@ import XMonad
 import XMonad.Layout.NoBorders
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Util.EZConfig
-import XMonad.Prompt
-import XMonad.Prompt.ConfirmPrompt
 import XMonad.Actions.WindowBringer
-import XMonad.Prompt.Shell
-import System.Exit
 
 main = do
   xmonad $ ewmh def
@@ -17,23 +13,12 @@ main = do
 
      `additionalKeysP` -- Add some extra key bindings:
       [ 
-        ("M-S-q",   confirmPrompt myXPConfig "exit" (io exitSuccess))
-      , ("M-p",     shellPrompt myXPConfig)
-      , ("M-S-g",   gotoMenu)
+        ("M-S-g",   gotoMenu)
       , ("M-S-b",   bringMenu)
       , ("M-w", spawn "/home/mindaugas/.scripts/screeny")
       , ("M-r", spawn "/home/mindaugas/.scripts/shutdown.sh")
       , ("M-x", spawn "xclock -d")
       ]
---------------------------------------------------------------------------------
 
-myXPConfig = def
-  { position          = Top
-  , alwaysHighlight   = True
-  ,fgColor            = "#657b83"
-  , bgColor           = "#002b36"
-  , bgHLight    = "#002b36"
-  , fgHLight    = "#dc322f"
-  , promptBorderWidth = 0
-  , font              = "xft:Liberation Sans:size=10"
-  }
+
+
